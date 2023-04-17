aa1dd11779c6e56ffb57e2f2f5c0196231899d58
Luigi Iossa <luigi_iossa@hotmail.com>
2023-04-17 16:38:56 +0000

    modifiche fatte

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import store from './Redux/Store/store';
 import MyNav from './Components/MyNav';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ProfileJumbotron from './Components/ProfileJumbotron';
+import MessageBox from './Components/MeesageBox';
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
      <MyNav/>
-
+     <ProfileJumbotron/>
+     <MessageBox/>
+    </div>
     </Provider>
+)
 }
 
 export default App;
