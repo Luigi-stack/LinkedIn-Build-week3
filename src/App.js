ca1a036c3cce37b74c00286eeab16597c627caa6
Luigi Iossa <luigi_iossa@hotmail.com>
2023-04-17 13:01:14 +0000

    icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './Redux/Store/store';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ProfileJumbotron from './Components/ProfileJumbotron';
+
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
-     
+
+    <ProfileJumbotron/>
+
     </div>
     </Provider>
   );
