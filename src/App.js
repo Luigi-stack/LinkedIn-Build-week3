046d76a12a102c58a0fc6b30d0b23f02fed923f0
[nome] <davidscattone10@gmail.com>
2023-04-17 18:10:13 +0200

    messagebox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './Redux/Store/store';
+import MessageBox from './Components/MeesageBox';
+import'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
-     
+     <MessageBox />
     </div>
     </Provider>
   );
