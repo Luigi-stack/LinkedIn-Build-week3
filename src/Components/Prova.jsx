f9be901196d47ff53048f42e076315ec00cba98a
MarcoDeLuca2405 <Marco_De_luca2405@hotmail.com>
2023-04-18 12:10:41 +0200

    .

--- src/Components/Prova.jsx
+++ src/Components/Prova.jsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
-import { getUser } from "../Redux/Actions/action_profile";
+import { getUserMe } from "../Redux/Actions/action_profile";
 
 
 
 const Prova = () =>{
     const dispatch=useDispatch();
     const auterization="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNjZjNjNzE4NmE4NzAwMTQzODY3YjEiLCJpYXQiOjE2ODE3MzgyMDgsImV4cCI6MTY4Mjk0NzgwOH0.443vSkzyX21q7caKjjQyv2nT4WBXdOYyK9yu1mifDd0";
-    useEffect(()=>{dispatch(getUser(auterization))},[])
+    useEffect(()=>{dispatch(getUserMe(auterization))},[])
 return (
     <>
     </>
