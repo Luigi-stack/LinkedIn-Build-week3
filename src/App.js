2c8a90cd62240d11db55f05c1780684a044be12f
Luigi Iossa <luigi_iossa@hotmail.com>
2023-04-18 10:16:53 +0000

    modifiche

--- src/App.js
+++ src/App.js
@@ -13,10 +13,6 @@ function App() {
   return (
     <Provider store={store}>
     <div className="App">
-
-    <ProfileJumbotron/>
-    <Prova />
-  
      <MyNav/>
      <ProfileJumbotron/>
      <MessageBox/>
